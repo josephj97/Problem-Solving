99fbabc6bc943f65d0af80ebe500a92e83463bce
Joseph Saad <josephatef.j97@gmail.com>
2020-05-12 12:27:20 +0200

    Accepted on codeforces 4A

---
+++ PHP/codeforces_4A.php
@@ -0,0 +1,9 @@
+<!-- https://codeforces.com/problemset/problem/4/A -->
+<?php
+    $a = (int)readline();
+    if ($a%2 == 0 && $a>2){
+        echo "YES";
+    } else{
+        echo "NO";
+    }
+?>
